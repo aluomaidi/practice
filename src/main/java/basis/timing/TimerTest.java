65aad9b32e628cde0ffeeb2bca0e27faaa04e9b8
aluomaidi <731502518@qq.com>
2016-02-04 14:44:56 +0800

    顺便测试线程调用run,start方法的区别

--- src/main/java/basis/timing/TimerTest.java
+++ src/main/java/basis/timing/TimerTest.java
@@ -20,17 +20,22 @@ public class TimerTest {
             public void run()
             {
 
-                System.out.println("task1 invoked ! "
+                /*System.out.println("task1 invoked ! "
                         + (System.currentTimeMillis() - start));
                 try
                 {
+                // 这里休眠的是TimerThread,执行线程，而不是当前TimerTask，
+                // 因为TimerTask只是普通对象不是线程
                     Thread.sleep(3000);
                     System.out.println(Thread.currentThread().getName());
                 } catch (InterruptedException e)
                 {
                     e.printStackTrace();
+                }*/
+                int size = Integer.MAX_VALUE>>1000;
+                for (int i=1;i<size;i++) {
+                    System.out.println(i);
                 }
-
             }
         };
         TimerTask task2 = new TimerTask()
@@ -46,6 +51,16 @@ public class TimerTest {
         start = System.currentTimeMillis();
         timer.schedule(task1, 1000);
         timer.schedule(task2, 3000);
+      /*  //定时器只是实现runnable接口，并不是线程
+        task1.run();
+        task2.run();*/
+       /*    //调用run方法只是调用普通成员方法，并没有启动线程
+        new Thread(task1).run();
+        new Thread(task2).run();*/
+      /*  //这样调用才会启动线程
+        new Thread(task1).start();
+        new Thread(task2).start();*/
+
 
     }
 }
