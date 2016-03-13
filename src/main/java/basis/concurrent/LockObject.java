1f86a2ed99270716d39c9784cfc4d845aa2db333
aluomaidi <731502518@qq.com>
2016-03-13 23:30:26 +0800

    锁是真是实例对象的，所以两个线程操作一个类的两个实例是并发的

--- src/main/java/basis/concurrent/LockObject.java
+++ src/main/java/basis/concurrent/LockObject.java
@@ -4,14 +4,19 @@ package basis.concurrent;
  * Created by luozj on 16/3/13.
  */
 public class LockObject {
-    int value;
-    public synchronized int getAndSet () {
-        return ++value;
+    public synchronized void print1 (int count) {
+        int i = 0;
+        while (i<count) {
+            System.out.print(1);
+            i++;
+        }
     }
-    public synchronized void set (int _value) {
-        value =_value;
-    }
-    public synchronized int get () {
-        return value;
+
+    public synchronized void print2 (int count) {
+        int i = 0;
+        while (i<count) {
+            System.out.print(2);
+            i++;
+        }
     }
 }
--- src/main/java/basis/concurrent/SynchronizedTest.java
+++ src/main/java/basis/concurrent/SynchronizedTest.java
@@ -4,31 +4,20 @@ package basis.concurrent;
  * Created by luozj on 16/3/13.
  */
 public class SynchronizedTest {
-    // 锁是真是类对象的，所以两个线程操作一个类的两个实例依然是互斥的
+    // 锁是真是实例对象的，所以两个线程操作一个类的两个实例是并发的
 
     public static void main(String[] args) {
         final LockObject obj1 = new LockObject();
         final LockObject obj2 = new LockObject();
         Thread thread1 = new Thread(new Runnable() {
             public void run() {
-                int i =1;
-                while(i<10){
-                    obj1.getAndSet();
-                    System.out.println(obj1.get());
-                    i++;
-                }
+                    obj1.print1(1000);
             }
         });
         thread1.start();
         Thread thread2 = new Thread(new Runnable() {
             public void run() {
-                int i =1;
-                while(i<10){
-                    obj2.set(-1);
-                    System.out.println(obj2.get());
-                    i++;
-                }
-
+                    obj2.print2(1000);
             }
         });
         thread2.start();
