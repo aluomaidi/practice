ed17dc57693296054f265f2d78b54491a054fa8d
aluomaidi <731502518@qq.com>
2016-06-01 17:30:12 +0800

    移位测试

---
+++ src/main/java/basis/bitmove/BitMove.java
@@ -0,0 +1,21 @@
+package basis.bitmove;
+
+/**
+ * Created by admin on 2016/6/1.
+ */
+public class BitMove {
+    public static void main(String[] args) {
+        int i = 2;
+        int j = -1;
+        int k = -2; // 负数是补码
+        System.out.println(i<<1);
+        System.out.println(i>>1);
+        System.out.println(i>>>1);
+        System.out.println(j>>1);
+        System.out.println(j<<1);
+        System.out.println(j>>>1);
+        System.out.println(k>>1);
+        System.out.println(k<<1);
+        System.out.println(k>>>1);
+    }
+}
